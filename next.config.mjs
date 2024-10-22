3001b4d37a2d2c698282162be2076f8bd35783f9
Zhandos <zh.zhanybek@gmail.com>
2024-10-22 11:22:13 +0500

    setup next.js environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@node': path.resolve('node_modules'), 
+    };
+    return config;
+  }
+};
+
 
 export default nextConfig;
