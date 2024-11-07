e0777c5bec09cc405135c7033b9a8162b3a09152
Zhandos <zh.zhanybek@gmail.com>
2024-11-07 17:45:43 +0500

    feat: add favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Heebo } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
@@ -11,9 +11,9 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./../globals.scss";
 
-const heebo = Heebo({
+const heebo = Open_Sans({
   subsets: ["latin"],
-  weight: ["400", "500", "700", "900"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -35,6 +35,23 @@ export default async function RootLayout({
   const messages = await getMessages();
   return (
     <html lang={locale}>
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="zhanda.dev" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body className={heebo.className}>
         <NextIntlClientProvider messages={messages}>
           <Header />
