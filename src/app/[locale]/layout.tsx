90dd9c65fe81a69319c6b9736719704af5f05df4
Zhandos <zh.zhanybek@gmail.com>
2024-11-09 02:43:53 +0500

    feat: add search metrics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -50,6 +50,8 @@ export default async function RootLayout({
         />
         <meta name="apple-mobile-web-app-title" content="zhanda.dev" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="google-site-verification" content="9uC-dOThG2blBEVeSDS4B_mK0dbeLMLoy75bFrJIg5A" />
+        <meta name="yandex-verification" content="2251d91dd91fb1d5" />
       </head>
       <body className={heebo.className}>
         <NextIntlClientProvider messages={messages}>
