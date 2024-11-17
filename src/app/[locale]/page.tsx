fb47e9dd0bead6bdb5796979d1a60cf7939e60af
Zhandos <zh.zhanybek@gmail.com>
2024-11-18 02:49:33 +0500

    fix: fix href for cv

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,6 @@ import { getTranslations } from "next-intl/server";
 import { Link } from "@/i18n/routing";
 import Image from "next/image";
 import { setRequestLocale } from "next-intl/server";
-import { localizations } from "@/i18n/localizations";
 
 export const dynamic = "force-static";
 
@@ -55,7 +54,7 @@ export default async function Home({
                 <p className="about-me__text">{t("about-me")}</p>
                 <div className="about-me__buttons">
                   <a
-                    href={`/files/Zhanybek_Zhandos_CV_${localizations[locale]}.pdf?01.01.2024`}
+                    href={`/files/Zhanybek_Zhandos_CV_${locale}.pdf?18.11.24`}
                     download
                     className="about-me__button btn btn-primary"
                   >
